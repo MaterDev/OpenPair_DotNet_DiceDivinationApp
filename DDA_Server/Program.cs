a2e7d6f8bdcf5e6dd37968dd656dc9d99c835234
Key Clark <aryeh.the.lion@gmail.com>
2023-12-20 15:01:11 -0600

    intialize and add comments to basic server

--- DDA_Server/Program.cs
+++ DDA_Server/Program.cs
@@ -1,3 +1,5 @@
+//! --- 👾 Basic Configurations for app and swagger build ---
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,28 +18,37 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+//! --- 👾 Routes ---
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+// Define a GET route to return weather
 app.MapGet("/weatherforecast", () =>
 {
+    // Create 5 forecasts, and for each of them create a new WeatherForecast
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)), // Will become the date, which is current day + index
+            Random.Shared.Next(-20, 55), // Will choose a temp (c) from 20 - 55
+            summaries[Random.Shared.Next(summaries.Length)] // choosing a random item from the array, accounting for the length of the array as the maximum index number to choose from.
         ))
-        .ToArray();
+        .ToArray(); // Casting a record into an array of values.
     return forecast;
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+
+//! Run the server
 app.Run();
 
+
+//! This record defines the value schema for a weather forecast, when a new forecast is created the incoming parameters become the properties, and variables that are defined also become properties accessible. 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
