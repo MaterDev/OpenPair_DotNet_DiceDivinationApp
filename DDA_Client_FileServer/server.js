ffa1db707fcd7f61d1917a202669d6b0201e4b1f
Mater Dev <aryeh.the.lion@gmail.com>
2024-01-31 14:24:45 -0600

    added prefix to file server endpoints

--- DDA_Client_FileServer/server.js
+++ DDA_Client_FileServer/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any available port
+const port = 3000; 
 
 // Serve static files from a specified directory, e.g., 'public'
-app.use('/images', express.static('./storage/images/'));
+app.use('/fs/images', express.static('./storage/images/'));
 
 // Specifically for rolled images
-app.use('/rolledImages', express.static('./storage/images/diceSpreadRolledImages'));
+app.use('/fs/rolledImages', express.static('./storage/images/diceSpreadRolledImages'));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
